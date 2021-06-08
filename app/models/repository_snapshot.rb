826b8d5a0121054a11d7104d266eaccbb0ee4051
David Backeus <duztdruid@gmail.com>
2021-06-08 17:22:00 +0200

    Fix repository deletion performance bottleneck

--- app/models/repository_snapshot.rb
+++ app/models/repository_snapshot.rb
@@ -1,5 +1,9 @@
+# Note: don't rely on destroy callbacks since model entries are deleted via
+# foreign key delete cascade
+
 class RepositorySnapshot < ApplicationRecord
   belongs_to :repository
 
-  has_one :repository_snapshot_tokei_dump, dependent: :delete
+  has_one :repository_snapshot_tokei_dump # dependent: delete via foreign key
+  alias :tokei_dump :repository_snapshot_tokei_dump
 end
--- test/models/repository_test.rb
+++ test/models/repository_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class RepositoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "applies ON DELETE CASCADE to snapshots + dumps to avoid N+1 dependent: destroy performance bottleneck" do
+    repository = repositories(:small)
+
+    snapshots = repository.repository_snapshots
+    tokei_dumps = snapshots.map(&:tokei_dump)
+
+    repository.destroy
+
+    refute RepositorySnapshot.where(id: snapshots.pluck(:id)).exists?
+    refute RepositorySnapshotTokeiDump.where(id: tokei_dumps.pluck(:id)).exists?
+  end
 end
